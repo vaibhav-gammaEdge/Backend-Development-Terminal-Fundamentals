eb5e41b3479b47c8ef2326c4492a4af81a8b8f41
“Vaibhav <vaibhav.joswal@gammaedge.io>
2026-01-21 18:15:51 +0530

    changes made

---
+++ Day2/test.py
@@ -0,0 +1,14 @@
+import requests 
+
+url = "https://api.github.com/repos/vaibhav-gammaEdge/Backend-Development-Terminal-Fundamentals"
+
+response = requests.get(url)
+
+print("Status Code:", response.status_code)
+
+data = response.json()
+
+print("Repository Name:", data["name"])
+print("Owner Username:", data["owner"]["login"])
+print("Stars:", data["stargazers_count"])
+print("Forks:", data["forks_count"])
